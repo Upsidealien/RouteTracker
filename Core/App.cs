a09abab0bf9846284f8ba961baee7084d4386acc
Thomas Cartwright <TJCARTWRIGHT91@GMAIL.COM>
2017-03-18 13:20:15 +1100

    initial commit

---
+++ Core/App.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace FormsBackgrounding
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			MainPage = new LongRunningPage();
+		}
+
+		protected override void OnStart ()
+		{
+		}
+
+		protected override void OnSleep ()
+		{
+		}
+
+		protected override void OnResume ()
+		{
+		}
+			
+	}
+}
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,40 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using System;
+using FormsBackgrounding.Messages;
+
+namespace FormsBackgrounding.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+
+		#region Methods
+		iOSLongRunningTaskExample longRunningTaskExample;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			Forms.Init ();
+
+			LoadApplication (new App ());
+
+			WireUpLongRunningTask ();
+
+			return base.FinishedLaunching (app, options);
+		}
+
+		void WireUpLongRunningTask ()
+		{
+			MessagingCenter.Subscribe<StartLongRunningTaskMessage> (this, "StartLongRunningTaskMessage", async message => {
+				longRunningTaskExample = new iOSLongRunningTaskExample ();
+				await longRunningTaskExample.Start ();
+			});
+
+			MessagingCenter.Subscribe<StopLongRunningTaskMessage> (this, "StopLongRunningTaskMessage", message => {
+				longRunningTaskExample.Stop ();
+			});
+		}
+		#endregion
+	}
+}
